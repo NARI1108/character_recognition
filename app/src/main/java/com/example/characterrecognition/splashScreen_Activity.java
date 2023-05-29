2c780de35daec2cae18c81cf8af33dcd9d810ab5
Hill <125409221+NARI1108@users.noreply.github.com>
2023-05-29 21:42:33 +0330

    Adding ids and animation method and connecting ids to this activity.

--- app/src/main/java/com/example/characterrecognition/splashScreen_Activity.java
+++ app/src/main/java/com/example/characterrecognition/splashScreen_Activity.java
@@ -5,14 +5,19 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.LinearLayout;
 
 public class splashScreen_Activity extends AppCompatActivity {
-
+    LinearLayout img_splashScreen;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
         splashScreen();
+        findViews();
+        animation();
     }
     public void splashScreen(){
         new Handler().postDelayed(new Runnable() {
@@ -21,6 +26,14 @@ public class splashScreen_Activity extends AppCompatActivity {
                 startActivity(new Intent(splashScreen_Activity.this,MainActivity.class));
                 finish();
             }
-        },4000);
+        },6000);
+    }
+    public void findViews(){
+        img_splashScreen = findViewById(R.id.img_splashScreen);
+    }
+    public void animation(){
+        Animation animation_splashScreen = AnimationUtils.loadAnimation(this,R.anim.alpha);
+
+        img_splashScreen.setAnimation(animation_splashScreen);
     }
 }
