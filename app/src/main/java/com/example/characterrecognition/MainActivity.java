f0db29d55a4db735be8e01383f42e9aaecaad105
Hill <125409221+NARI1108@users.noreply.github.com>
2023-05-31 23:21:14 +0330

    Creating a method(onClick) for click on the buttons and adding picture for icon and implementing class MainActivity.java

--- app/src/main/java/com/example/characterrecognition/MainActivity.java
+++ app/src/main/java/com/example/characterrecognition/MainActivity.java
@@ -2,11 +2,16 @@ package com.example.characterrecognition;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.Button;
-public class MainActivity extends AppCompatActivity {
+import android.widget.LinearLayout;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
     Button btn_test, btn_about_us, btn_contact_us, btn_exit;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,6 +19,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         findViews();
         animation();
+        setOnClickListener();
     }
     public void findViews(){
         btn_test = findViewById(R.id.btn_test);
@@ -31,5 +37,26 @@ public class MainActivity extends AppCompatActivity {
         btn_about_us.setAnimation(animation_btn_about_us);
         btn_contact_us.setAnimation(animation_btn_contact_us);
         btn_exit.setAnimation(animation_btn_exit);
+
+    }
+    public void setOnClickListener(){
+        btn_test.setOnClickListener(this);
+        btn_about_us.setOnClickListener(this);
+        btn_contact_us.setOnClickListener(this);
+        btn_exit.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        switch(view.getId()){
+            case R.id.btn_test:
+                startActivity(new Intent(this,testing_Activity.class));
+                break;
+            case R.id.btn_about_us:
+                new AlertDialog.Builder(this).setTitle(R.string.about_us).setMessage(R.string.about_us_txt).setIcon(R.drawable.icon).show();
+                break;
+            case R.id.btn_exit:finish();
+        }
     }
 }
