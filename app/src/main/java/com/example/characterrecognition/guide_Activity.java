e70300f5d6af0e00848405255db029a7970f4fb2
Hill <125409221+NARI1108@users.noreply.github.com>
2023-06-18 23:10:22 +0330

    Creating a video for the app and setting its playback controls.

--- app/src/main/java/com/example/characterrecognition/guide_Activity.java
+++ app/src/main/java/com/example/characterrecognition/guide_Activity.java
@@ -2,9 +2,11 @@ package com.example.characterrecognition;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.MediaController;
 import android.widget.TextView;
 import android.widget.VideoView;
 
@@ -17,6 +19,7 @@ public class guide_Activity extends AppCompatActivity {
         setContentView(R.layout.activity_guide);
         findViews();
         animation();
+        video();
     }
     public void findViews(){
         txt_title  = findViewById(R.id.txt_title);
@@ -28,5 +31,17 @@ public class guide_Activity extends AppCompatActivity {
 
         txt_title.setAnimation(animation_txt_title);
         video_view.setAnimation(animation_video_view);
+    }
+    public void video(){
+        video_view.setVideoPath("android.resource://"+getPackageName()+"/"+R.raw.video_help);
+        MediaController mediaController = new MediaController(this);
+        mediaController.setAnchorView(video_view);
+        video_view.setMediaController(mediaController);
+        video_view.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mediaPlayer) {
+                video_view.start();
+            }
+        });
     }
 }
