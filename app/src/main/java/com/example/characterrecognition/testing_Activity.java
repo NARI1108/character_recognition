e6ef18e3b5534d0a30b03f63c6238c46ec75539a
Hill <125409221+NARI1108@users.noreply.github.com>
2023-06-25 22:16:32 +0330

    Creating animation for Views at testing_Activity.java

--- app/src/main/java/com/example/characterrecognition/testing_Activity.java
+++ app/src/main/java/com/example/characterrecognition/testing_Activity.java
@@ -3,6 +3,8 @@ package com.example.characterrecognition;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.TextView;
 
 public class testing_Activity extends AppCompatActivity {
@@ -12,6 +14,7 @@ public class testing_Activity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_testing);
         findViews();
+        animation();
     }
     public void findViews(){
         txt_counter = findViewById(R.id.txt_counter);
@@ -20,5 +23,20 @@ public class testing_Activity extends AppCompatActivity {
         txt_question3 = findViewById(R.id.txt_question3);
         txt_question4 = findViewById(R.id.txt_question4);
         txt_question5 = findViewById(R.id.txt_question5);
+    }
+    public void animation(){
+        Animation animation_txt_counter = AnimationUtils.loadAnimation(this,R.anim.alpha);
+        Animation animation_txt_question1 = AnimationUtils.loadAnimation(this,R.anim.translate_right);
+        Animation animation_txt_question2 = AnimationUtils.loadAnimation(this,R.anim.translate_right);
+        Animation animation_txt_question3 = AnimationUtils.loadAnimation(this,R.anim.translate_right);
+        Animation animation_txt_question4 = AnimationUtils.loadAnimation(this,R.anim.translate_right);
+        Animation animation_txt_question5 = AnimationUtils.loadAnimation(this,R.anim.translate_right);
+
+        txt_counter.setAnimation(animation_txt_counter);
+        txt_question1.setAnimation(animation_txt_question1);
+        txt_question2.setAnimation(animation_txt_question2);
+        txt_question3.setAnimation(animation_txt_question3);
+        txt_question4.setAnimation(animation_txt_question4);
+        txt_question5.setAnimation(animation_txt_question5);
     }
 }
